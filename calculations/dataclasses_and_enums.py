23b0318039d172355152d2a423c70458ec51df03
kenyon-allan <allan.k@husky.neu.edu>
2024-06-05 08:29:23 -0400

    clone function

--- calculations/dataclasses_and_enums.py
+++ calculations/dataclasses_and_enums.py
@@ -1,5 +1,6 @@
 from enum import StrEnum
 from dataclasses import dataclass
+from typing import Self
 
 
 class SevenTalents(StrEnum):
@@ -30,6 +31,24 @@ class SamuroCounters:
     remaining_w_cd: int = 0  # remaining W cooldown, defaults to 10 seconds
     crit_counter: int = 0  # counter for crits - we crit every 4th attack by default and every 3rd with WOTB
     cb_counter: int = 0  # Stacks of crushing blows. We start at 1 because we began with a preloaded crit.
+    clone: bool = (
+        False  # Clones don't benefit from crushing blows, and can't trigger w, so we need to explicitly track them.
+    )
+
+    def create_clone(self: Self, level: int) -> "SamuroCounters":
+        """Simulates a clone of Samuro which has fractions of his damage, but maintain crit stacks."""
+        clone_base_damage = 11  # clones suck wow.
+        clone = SamuroCounters(
+            aa_damage=clone_base_damage * (1.04**level),
+            crit_damage=clone_base_damage * (1.04**level) * 1.5,
+            aa_speed=self.aa_speed,
+            attack_cadence=self.attack_cadence,
+            remaining_w_cd=0,
+            crit_counter=self.crit_counter,
+            cb_counter=0,
+            clone=True,
+        )
+        return clone
 
 
 @dataclass
